00cb73b6eb39036ca945cd9c44971a6071eccb7c
Gabriel Caliari <124197851+GabrielCaliari@users.noreply.github.com>
2025-01-15 13:11:05 -0300

    inserindo o sistema de rotas no app

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignIn from '../pages/SignIn';
+
+const App = createNativeStackNavigator();
+
+const AppRoutes = () => {
+  return (
+    <App.Navigator initialRouteName='SignIn'>
+      <App.Screen name='SignIn' component={SignIn} options={{headerShown: false}} />
+      <App.Screen name='Home' component={Home} options={{headerShown: false}} />
+      <App.Screen name='Model' component={Model} options={{headerShown: false}} />
+    </App.Navigator>
+)
+}
+
+
+
+export default AppRoutes;
